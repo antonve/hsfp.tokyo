e4477feb697d8b810f2ece0960095409613d690b
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2021-05-05 09:26:28 +0900

    add i18n setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,16 @@
+import { useTranslation } from 'next-i18next'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+
 function Home() {
-  return (
-    <div>
-      Hello hsfp.tokyo
-    </div>
-  )
+  const { t } = useTranslation('common')
+  return <div>{t('intro', { siteName: 'hsfp.tokyo' })}</div>
 }
 
+export const getStaticProps = async ({ locale }: { locale: string }) => ({
+  props: {
+    ...(await serverSideTranslations(locale, ['common', 'footer'])),
+  },
+})
+
 export default Home
+
