471e641fed79d6300477a66dde912b1d09e2de48
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2021-06-12 16:51:16 +0900

    pass in visa type to academic background

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,15 +3,16 @@ import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import Layout from '@app/components/Layout'
 import VisaSelect from '@app/components/VisaSelect'
 import AcademicBackground from '@app/components/AcademicBackground'
+import { VisaType } from '@app/domain'
 
 function Home() {
   const { t } = useTranslation('common')
   return (
     <Layout>
       {t('intro', { siteName: 'hsfp.tokyo' })}
-      <div>
+      <div className={`flex flex-col space-y-8`}>
         <VisaSelect />
-        <AcademicBackground />
+        <AcademicBackground visaType={VisaType.B} />
       </div>
     </Layout>
   )
