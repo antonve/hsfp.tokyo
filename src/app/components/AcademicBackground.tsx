dfa2b0b736984de71350926ecf88f01d5d843bf0
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2021-06-12 20:47:03 +0900

    add form section to academic background

--- src/app/components/AcademicBackground.tsx
+++ src/app/components/AcademicBackground.tsx
@@ -3,7 +3,10 @@ import { useTranslation } from 'next-i18next'
 
 import { Qualification, VisaType } from '@app/domain'
 import { QualificationIds } from '@app/visa/b'
-import QualificationList, { QualificationOption } from './QualificationList'
+import QualificationList, {
+  QualificationOption,
+} from '@app/components/QualificationList'
+import FormSection from '@app/components/FormSection'
 
 const ids = QualificationIds.AcademicBackground
 
@@ -31,24 +34,29 @@ const AcademicBackground: FC<Props> = ({
     toggleQualification(id, createNewQualification)
 
   return (
-    <QualificationList>
-      <QualificationOption
-        qualifications={qualifications}
-        id={ids.doctor}
-        onChange={onChange}
-      >
-        <h3>{t('academicBackground.doctor.name')}</h3>
-        <p>{t('academicBackground.doctor.description')}</p>
-      </QualificationOption>
-      <QualificationOption
-        qualifications={qualifications}
-        id={ids.master}
-        onChange={onChange}
-      >
-        <h3>{t('academicBackground.master.name')}</h3>
-        <p>{t('academicBackground.master.description')}</p>
-      </QualificationOption>
-    </QualificationList>
+    <FormSection
+      title={t('simulation.academicBackground.title')}
+      description={t('simulation.academicBackground.description')}
+    >
+      <QualificationList>
+        <QualificationOption
+          qualifications={qualifications}
+          id={ids.doctor}
+          onChange={onChange}
+        >
+          <h3>{t('academicBackground.doctor.name')}</h3>
+          <p>{t('academicBackground.doctor.description')}</p>
+        </QualificationOption>
+        <QualificationOption
+          qualifications={qualifications}
+          id={ids.master}
+          onChange={onChange}
+        >
+          <h3>{t('academicBackground.master.name')}</h3>
+          <p>{t('academicBackground.master.description')}</p>
+        </QualificationOption>
+      </QualificationList>
+    </FormSection>
   )
 }
 
