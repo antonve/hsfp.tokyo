6817dfcc9367cbd54d459ba6949c7fbf6b478f22
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2021-06-13 16:04:48 +0900

    add number input component

--- src/app/components/Form.tsx
+++ src/app/components/Form.tsx
@@ -23,3 +23,20 @@ export const RadioButton: FC<{
     onChange={() => onChange(!value)}
   />
 )
+
+export const NumberInput: FC<{
+  value: number
+  onChange: (value: number) => void
+  step?: number
+}> = ({ value, onChange, step }) => (
+  <input
+    type="number"
+    className={`rounded`}
+    value={value}
+    onChange={event => {
+      const updatedValue = parseInt(event.target.value, 10)
+      onChange(isNaN(updatedValue) ? 0 : updatedValue)
+    }}
+    step={step ?? 1}
+  />
+)
