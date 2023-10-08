960a3f754670e975996e7099aa258d402359b39d
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2023-10-08 17:09:59 +0900

    disable licenses

--- src/components/Licenses.tsx
+++ src/components/Licenses.tsx
@@ -1,75 +1,75 @@
-import { FC } from 'react'
-import { useTranslation } from 'next-i18next'
+// import { FC } from 'react'
+// import { useTranslation } from 'next-i18next'
 
-import { LicensesQualification, Qualification, VisaType } from '@lib/domain'
-import QualificationList, {
-  QualificationRadio,
-} from '@components/QualificationList'
-import FormSection from '@components/FormSection'
+// import { LicensesQualification, Qualification, VisaType } from '@lib/domain'
+// import QualificationList, {
+//   QualificationRadio,
+// } from '@components/QualificationList'
+// import FormSection from '@components/FormSection'
 
-interface Props {
-  visaType: VisaType
-  qualifications: Qualification[]
-  selectQualificationForCategory: (qualification: Qualification) => void
-}
+// interface Props {
+//   visaType: VisaType
+//   qualifications: Qualification[]
+//   selectQualificationForCategory: (qualification: Qualification) => void
+// }
 
-type MatchFunction = (qualification: Qualification) => boolean
+// type MatchFunction = (qualification: Qualification) => boolean
 
-const Licenses: FC<Props> = ({
-  qualifications,
-  selectQualificationForCategory,
-}) => {
-  const { t } = useTranslation()
+// const Licenses: FC<Props> = ({
+//   qualifications,
+//   selectQualificationForCategory,
+// }) => {
+//   const { t } = useTranslation()
 
-  const createNewQualification = (count: number): LicensesQualification => ({
-    category: 'LICENSES',
-    id: 'licenses',
-    count,
-  })
+//   const createNewQualification = (count: number): LicensesQualification => ({
+//     category: 'LICENSES',
+//     id: 'licenses',
+//     count,
+//   })
 
-  const onChangeFor = (count: number) => () =>
-    selectQualificationForCategory(createNewQualification(count))
+//   const onChangeFor = (count: number) => () =>
+//     selectQualificationForCategory(createNewQualification(count))
 
-  const matcherFor =
-    (count: number): MatchFunction =>
-    (qualification: Qualification) =>
-      qualification.category === 'LICENSES' &&
-      qualification.id === 'licenses' &&
-      (qualification as LicensesQualification).count === count
+//   const matcherFor =
+//     (count: number): MatchFunction =>
+//     (qualification: Qualification) =>
+//       qualification.category === 'LICENSES' &&
+//       qualification.id === 'licenses' &&
+//       (qualification as LicensesQualification).count === count
 
-  return (
-    <FormSection
-      title={t('simulation.licenses.title')}
-      description={t('simulation.licenses.description')}
-    >
-      <QualificationList>
-        <QualificationRadio
-          qualifications={qualifications}
-          onChange={onChangeFor(0)}
-          match={matcherFor(0)}
-        >
-          <h3>{t('licenses.none.name')}</h3>
-          <p>{t('licenses.none.description')}</p>
-        </QualificationRadio>
-        <QualificationRadio
-          qualifications={qualifications}
-          onChange={onChangeFor(1)}
-          match={matcherFor(1)}
-        >
-          <h3>{t('licenses.one.name')}</h3>
-          <p>{t('licenses.one.description')}</p>
-        </QualificationRadio>
-        <QualificationRadio
-          qualifications={qualifications}
-          onChange={onChangeFor(2)}
-          match={matcherFor(2)}
-        >
-          <h3>{t('licenses.two_or_more.name')}</h3>
-          <p>{t('licenses.two_or_more.description')}</p>
-        </QualificationRadio>
-      </QualificationList>
-    </FormSection>
-  )
-}
+//   return (
+//     <FormSection
+//       title={t('simulation.licenses.title')}
+//       description={t('simulation.licenses.description')}
+//     >
+//       <QualificationList>
+//         <QualificationRadio
+//           qualifications={qualifications}
+//           onChange={onChangeFor(0)}
+//           match={matcherFor(0)}
+//         >
+//           <h3>{t('licenses.none.name')}</h3>
+//           <p>{t('licenses.none.description')}</p>
+//         </QualificationRadio>
+//         <QualificationRadio
+//           qualifications={qualifications}
+//           onChange={onChangeFor(1)}
+//           match={matcherFor(1)}
+//         >
+//           <h3>{t('licenses.one.name')}</h3>
+//           <p>{t('licenses.one.description')}</p>
+//         </QualificationRadio>
+//         <QualificationRadio
+//           qualifications={qualifications}
+//           onChange={onChangeFor(2)}
+//           match={matcherFor(2)}
+//         >
+//           <h3>{t('licenses.two_or_more.name')}</h3>
+//           <p>{t('licenses.two_or_more.description')}</p>
+//         </QualificationRadio>
+//       </QualificationList>
+//     </FormSection>
+//   )
+// }
 
-export default Licenses
+// export default Licenses
