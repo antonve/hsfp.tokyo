621fd339cc043a6a6fa31ec8f9d13a4f5a275c4f
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2023-10-08 17:01:45 +0900

    add font file

---
+++ src/app/fonts.ts
@@ -0,0 +1,12 @@
+import { Inter, Cormorant_Garamond } from '@next/font/google'
+
+export const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+})
+
+export const cormorantGaramond = Cormorant_Garamond({
+  weight: '300',
+  subsets: ['latin'],
+  variable: '--font-cormorant-garamond',
+})
