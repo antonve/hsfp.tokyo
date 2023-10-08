1457e8db6b27532c0c435859b82cc60004014092
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2023-10-08 17:02:29 +0900

    disable qualificationm list

--- src/components/QualificationList.tsx
+++ src/components/QualificationList.tsx
@@ -1,122 +1,122 @@
-import { FC } from 'react'
+// import { FC } from 'react'
 
-import {
-  containsMatchingQualification,
-  containsQualificationWithId,
-  getQualification,
-  Qualification,
-} from '@lib/domain'
-import { Checkbox, NumberInput, RadioButton } from '@components/Form'
-import classNames from 'classnames'
+// import {
+//   containsMatchingQualification,
+//   containsQualificationWithId,
+//   getQualification,
+//   Qualification,
+// } from '@lib/domain'
+// import { Checkbox, NumberInput, RadioButton } from '@components/Form'
+// import classNames from 'classnames'
 
-interface QualificationOptionProps {
-  qualifications: Qualification[]
-  id: string
-  onChange: (value: string) => void
-}
+// interface QualificationOptionProps {
+//   qualifications: Qualification[]
+//   id: string
+//   onChange: (value: string) => void
+// }
 
-export const QualificationOption: FC<QualificationOptionProps> = ({
-  children,
-  qualifications,
-  id,
-  onChange,
-}) => {
-  const checked = containsQualificationWithId(qualifications, id)
-  const containerClasses = classNames(
-    'py-2 px-4 flex cursor-pointer select-none',
-    {
-      'bg-indigo-400': checked,
-    },
-  )
+// export const QualificationOption: FC<QualificationOptionProps> = ({
+//   children,
+//   qualifications,
+//   id,
+//   onChange,
+// }) => {
+//   const checked = containsQualificationWithId(qualifications, id)
+//   const containerClasses = classNames(
+//     'py-2 px-4 flex cursor-pointer select-none',
+//     {
+//       'bg-indigo-400': checked,
+//     },
+//   )
 
-  return (
-    <label className={containerClasses}>
-      <div className="flex-grow">{children}</div>
-      <div className="flex content-center items-center">
-        <Checkbox value={checked} onChange={() => onChange(id)} />
-      </div>
-    </label>
-  )
-}
+//   return (
+//     <label className={containerClasses}>
+//       <div className="flex-grow">{children}</div>
+//       <div className="flex content-center items-center">
+//         <Checkbox value={checked} onChange={() => onChange(id)} />
+//       </div>
+//     </label>
+//   )
+// }
 
-interface QualificationNumberProps {
-  qualifications: Qualification[]
-  category: string
-  id: string
-  onChange: (value: number) => void
-  getValue: (qualification: Qualification | undefined) => number | undefined
-  step?: number
-  min?: number
-  max?: number
-}
+// interface QualificationNumberProps {
+//   qualifications: Qualification[]
+//   category: string
+//   id: string
+//   onChange: (value: number) => void
+//   getValue: (qualification: Qualification | undefined) => number | undefined
+//   step?: number
+//   min?: number
+//   max?: number
+// }
 
-export const QualificationNumber: FC<QualificationNumberProps> = ({
-  children,
-  qualifications,
-  id,
-  category,
-  onChange,
-  getValue,
-  step,
-  min,
-  max,
-}) => {
-  const qualification = getQualification(qualifications, category, id)
-  const value = getValue(qualification)
-  const containerClasses = classNames(
-    'py-2 px-4 flex cursor-pointer select-none',
-  )
+// export const QualificationNumber: FC<QualificationNumberProps> = ({
+//   children,
+//   qualifications,
+//   id,
+//   category,
+//   onChange,
+//   getValue,
+//   step,
+//   min,
+//   max,
+// }) => {
+//   const qualification = getQualification(qualifications, category, id)
+//   const value = getValue(qualification)
+//   const containerClasses = classNames(
+//     'py-2 px-4 flex cursor-pointer select-none',
+//   )
 
-  return (
-    <label className={containerClasses}>
-      <div className="flex-grow">{children}</div>
-      <div className="flex content-center items-center w-1/4">
-        <NumberInput
-          value={value}
-          onChange={onChange}
-          step={step}
-          min={min}
-          max={max}
-        />
-      </div>
-    </label>
-  )
-}
+//   return (
+//     <label className={containerClasses}>
+//       <div className="flex-grow">{children}</div>
+//       <div className="flex content-center items-center w-1/4">
+//         <NumberInput
+//           value={value}
+//           onChange={onChange}
+//           step={step}
+//           min={min}
+//           max={max}
+//         />
+//       </div>
+//     </label>
+//   )
+// }
 
-interface QualificationRadioProps {
-  qualifications: Qualification[]
-  match: (qualification: Qualification) => boolean
-  onChange: () => void
-}
+// interface QualificationRadioProps {
+//   qualifications: Qualification[]
+//   match: (qualification: Qualification) => boolean
+//   onChange: () => void
+// }
 
-export const QualificationRadio: FC<QualificationRadioProps> = ({
-  children,
-  qualifications,
-  match,
-  onChange,
-}) => {
-  const checked = containsMatchingQualification(qualifications, match)
-  const containerClasses = classNames(
-    'py-2 px-4 flex cursor-pointer select-none',
-    {
-      'bg-indigo-400': checked,
-    },
-  )
+// export const QualificationRadio: FC<QualificationRadioProps> = ({
+//   children,
+//   qualifications,
+//   match,
+//   onChange,
+// }) => {
+//   const checked = containsMatchingQualification(qualifications, match)
+//   const containerClasses = classNames(
+//     'py-2 px-4 flex cursor-pointer select-none',
+//     {
+//       'bg-indigo-400': checked,
+//     },
+//   )
 
-  return (
-    <label className={containerClasses}>
-      <div className="flex-grow">{children}</div>
-      <div className="flex content-center items-center">
-        <RadioButton value={checked} onChange={onChange} />
-      </div>
-    </label>
-  )
-}
+//   return (
+//     <label className={containerClasses}>
+//       <div className="flex-grow">{children}</div>
+//       <div className="flex content-center items-center">
+//         <RadioButton value={checked} onChange={onChange} />
+//       </div>
+//     </label>
+//   )
+// }
 
-const QualificationList: FC<{}> = ({ children }) => (
-  <div className="grid grid-cols-1 w-full rounded-md border-4 border-indigo-400 divide-y divide-y-8">
-    {children}
-  </div>
-)
+// const QualificationList: FC<{}> = ({ children }) => (
+//   <div className="grid grid-cols-1 w-full rounded-md border-4 border-indigo-400 divide-y divide-y-8">
+//     {children}
+//   </div>
+// )
 
-export default QualificationList
+// export default QualificationList
