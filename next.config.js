82ebae1e039e0b9908cd03142fd92ca2b384f35d
minh-ctrl <binhminh140899@gmail.com>
2023-10-19 14:42:51 +0900

    remove unecessary next-18n config file

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true, // Recommended for the `pages` directory, default in `app`.
-    swcMinify: true,
-}
+const nextConfig = {}
 
 module.exports = nextConfig
