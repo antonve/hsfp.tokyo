faf86ccd4ff5718c20113e5cda5e953e370b5a2a
minh-ctrl <binhminh140899@gmail.com>
2023-10-22 18:14:59 +0900

    remove uncessary passing language props

--- src/app/[language]/calculator/[visa]/page.tsx
+++ src/app/[language]/calculator/[visa]/page.tsx
@@ -6,13 +6,11 @@ import { RedirectType, redirect } from 'next/navigation'
 interface Props {
   params: {
     visa: string,
-    language: string,
   }
 }
 
 export default function Page({ params }: Props) {
   const formConfig = useFormConfig(params.visa)
-  const language = params?.language
   const category = formConfig.order[0]
-  redirect(`/${language}/calculator/${params.visa}/${category}/1`, RedirectType.replace)
+  redirect(`/calculator/${params.visa}/${category}/1`, RedirectType.replace)
 }
