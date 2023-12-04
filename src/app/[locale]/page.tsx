49789d16e0aa1cc3f70169bb85a086403fbb9854
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2023-12-04 21:42:05 +0900

    Add draft of home page

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,4 +1,9 @@
 import { Logo } from '@components/Logo'
+import {
+  BeakerIcon,
+  BriefcaseIcon,
+  WrenchScrewdriverIcon,
+} from '@heroicons/react/24/solid'
 import { useTranslations } from 'next-intl'
 import Link from 'next-intl/link'
 
@@ -8,32 +13,91 @@ export default function Page() {
   return (
     <>
       <div className="max-w-7xl mx-auto">
-        <header className="py-6 px-4 flex justify-between space-x-8">
+        <header className="py-6 px-4 flex justify-between space-x-8 border-b-4 border-stone-900/50">
           <Logo />
           <nav>
-            <ul className="flex space-x-4">
+            <ul className="flex space-x-4 items-center justify-center h-full font-semibold text-lg">
               <li>
-                <Link href={`/`}>{t('home')}</Link>
+                <Link
+                  href={`/`}
+                  className="no-underline border-b-2 border-red-500 hover:border-white/40"
+                >
+                  {t('home')}
+                </Link>
               </li>
               <li>
-                <Link href={`/start`}> {t('calculator')}</Link>
-              </li>
-              <li>
-                <Link href={`/about`}>{t('about')}</Link>
+                <Link href={`/about`} className="no-underline">
+                  {t('about')}
+                </Link>
               </li>
             </ul>
           </nav>
         </header>
 
-        <div>
-          <Link href={`/calculator/researcher`}>{t('start_researcher')}</Link>
-          <br />
-          <Link href={`/calculator/engineer`}>{t('start_engineer')}</Link>
-          <br />
-          <Link href={`/calculator/business-manager`}>
-            {t('start_business_manager')}
-          </Link>
-        </div>
+        <section className="max-w-7xl px-4">
+          <h1 className="mt-16 text-center text-5xl leading-normal font-bold tracking-tighter sm:text-6xl/normal md:text-7xl/normal">
+            Japan Highly Skilled Foreign Professional Visa Points Calculator
+          </h1>
+          <p className="text-center my-8 text-zinc-400 text-xl/relaxed lg:text-2xl/relaxed xl:text-3xl/relaxed">
+            Choose your visa category to start the points calculation.
+          </p>
+
+          <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3 text-center">
+            <Link
+              className="card bg-zinc-900 hover:bg-zinc-800 transition ease-in-out duration-200 shadow-xl p-4 rounded-lg no-underline"
+              href={`/calculator/researcher`}
+            >
+              <div className="card-body space-y-4">
+                <h3 className="text-2xl font-semibold flex justify-center items-center space-x-2">
+                  <BeakerIcon className="w-5 h-5" />
+                  <span>Researcher</span>
+                </h3>
+                <p className="text-zinc-400">
+                  Visa for academic research activities.
+                </p>
+                <div className="mt-10 rounded-md bg-zinc-600/50 py-2 px-4">
+                  Start points calculation
+                </div>
+              </div>
+            </Link>
+
+            <Link
+              className="card bg-zinc-900 hover:bg-zinc-800 transition ease-in-out duration-200 shadow-xl p-4 rounded-lg no-underline"
+              href={`/calculator/engineer`}
+            >
+              <div className="card-body space-y-4">
+                <h3 className="text-2xl font-semibold flex justify-center items-center space-x-2">
+                  <WrenchScrewdriverIcon className="w-5 h-5" />
+                  <span>Engineer</span>
+                </h3>
+                <p className="text-zinc-400">
+                  Visa for various engineering roles.
+                </p>
+                <div className="mt-10 rounded-md bg-zinc-600/50 py-2 px-4">
+                  Start points calculation
+                </div>
+              </div>
+            </Link>
+
+            <Link
+              className="card bg-zinc-900 hover:bg-zinc-800 transition ease-in-out duration-200 shadow-xl p-4 rounded-lg no-underline"
+              href={`/calculator/business-manager`}
+            >
+              <div className="card-body space-y-4">
+                <h3 className="text-2xl font-semibold flex justify-center items-center space-x-2">
+                  <BriefcaseIcon className="w-5 h-5" />
+                  <span>Business Manager</span>
+                </h3>
+                <p className="text-zinc-400">
+                  Visa for managerial positions in business.{' '}
+                </p>
+                <div className="mt-10 rounded-md bg-zinc-600/50 py-2 px-4">
+                  Start points calculation
+                </div>
+              </div>
+            </Link>
+          </div>
+        </section>
       </div>
     </>
   )
