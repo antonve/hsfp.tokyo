e9135619abf6e5be2d37cc10b46b7dc36e8f21c6
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2024-12-14 17:17:00 +0900

    fix fonts

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -3,10 +3,12 @@ import { Inter, Cormorant_Garamond } from 'next/font/google'
 export const inter = Inter({
   subsets: ['latin'],
   variable: '--font-inter',
+  display: 'swap',
 })
 
 export const cormorantGaramond = Cormorant_Garamond({
   weight: '300',
   subsets: ['latin'],
   variable: '--font-cormorant-garamond',
+  display: 'swap',
 })
